246eba1b244f6e810350bb08d85c4985468b517a
nbertrac <nbertrac@gmail.com>
2022-08-25 23:21:51 +0200

    connection faite

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:finalflutterapp/services/FirestoreHelper.dart';
+import 'package:finalflutterapp/services/constants.dart';
 
 void main() async{
   await WidgetsFlutterBinding.ensureInitialized();
@@ -13,21 +18,12 @@ void main() async{
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -38,14 +34,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -54,45 +42,135 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //variable
+  late String mail;
+  late String password;
 
-  void _incrementCounter() {
-    setState(() {
 
-      _counter++;
-    });
+  //fonction interne à la page
+  popUp(){
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context){
+          if(Platform.isIOS){
+            return CupertinoAlertDialog(
+              content: const Text("Adresse mail ou de de passe invalide"),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Ok")
+                )
+              ],
+            );
+          }
+          else {
+            return AlertDialog(
+              content: const Text("Adresse mail ou de de passe invalide"),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Ok")
+                )
+              ],
+            );
+          }
+        }
+
+    );
   }
-
   @override
   Widget build(BuildContext context) {
 
+
+
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
 
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          title: Text("Mon Bon Coin"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: bodyPage()
+
+    );
+  }
+
+
+  Widget bodyPage(){
+    return  Column(
+      children: [
+        const SizedBox(height: 10,),
+        TextField(
+          onChanged: (value){
+            setState(() {
+              mail = value;
+            });
+
+          },
+          decoration: InputDecoration(
+              fillColor: Colors.white,
+              filled: true,
+              hintText: "Entrer votre adresse mail",
+              border:  OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15)
+              )
+          ),
+        ),
+        const SizedBox(height: 10,),
+        TextField(
+          obscureText: true,
+          onChanged: (value){
+            setState(() {
+              password = value;
+            });
+
+          },
+          decoration: InputDecoration(
+              fillColor: Colors.white,
+              filled: true,
+              hintText: "Entrer votre mot de passe",
+              border:  OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15)
+              )
+          ),
+        ),
+        const SizedBox(height: 10,),
+        ElevatedButton(
+          onPressed: (){
+            FirestoreHelper().connected(mail, password).then((value){
+              MyAccount = value;
+              Navigator.push(context, MaterialPageRoute(
+                  builder: (context){
+                    return const Text('Dashboard');//DashBoard(mail: mail,password: password,);
+                  }
+              ));
+            }).catchError((onError){
+              //Afficher popUp
+              popUp();
+
+            });
+
+
+
+
+          },
+          child: const Text("Connexion"),
+        ),
+        TextButton(
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(
+                  builder: (context){
+                    return const Text("inscription");//Inscription();
+                  }
+              ));
+
+            },
+            child: const Text("Inscription"),
+        ),
+      ],
     );
   }
 }
